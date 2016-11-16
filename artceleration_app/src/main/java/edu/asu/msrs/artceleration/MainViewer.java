3efa5a7008a4f94456e661f6f0ef93722503a0b2
刘睿博 <cositan4lrb@gmail.com>
2016-11-16 02:50:11 -0700

    add bottom menu bar

--- artceleration_app/src/main/java/edu/asu/msrs/artceleration/MainViewer.java
+++ artceleration_app/src/main/java/edu/asu/msrs/artceleration/MainViewer.java
@@ -2,6 +2,7 @@ package edu.asu.msrs.artceleration;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CaptureRequest;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,9 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,6 +31,7 @@ public class MainViewer extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
+
     private Spinner spinner;
     private TextView status1;
     private TextView status2;
@@ -70,6 +75,8 @@ public class MainViewer extends AppCompatActivity {
             }
         });
 
+        initMenu();
+
         initSpinner();
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, testsArray);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -79,22 +86,68 @@ public class MainViewer extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 TransformTest t = tests[position];
-                if (artlib.requestTransform(src_img, t.transformType, t.intArgs, t.floatArgs)){
+                if (artlib.requestTransform(src_img, t.transformType, t.intArgs, t.floatArgs)) {
 
-                makeToast("Transform requested : "+ transforms[t.transformType]);
+                    makeToast("Transform requested : " + transforms[t.transformType]);
 
 
-                }else{
-                    makeToast("Transform request failed"+ transforms[t.transformType]);
+                } else {
+                    makeToast("Transform request failed" + transforms[t.transformType]);
                 }
             }
 
             @Override
-            public void onNothingSelected (AdapterView < ? > parent){
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
 
+    }
+
+    private void initMenu() {
+
+        AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
+
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("a", R.drawable.ic_maps_place, R.color.color_tab_1);
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("b", R.drawable.ic_maps_local_bar, R.color.color_tab_2);
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("c", R.drawable.ic_maps_local_restaurant, R.color.color_tab_3);
+
+        bottomNavigation.addItem(item1);
+        bottomNavigation.addItem(item2);
+        bottomNavigation.addItem(item3);
+
+        bottomNavigation.setDefaultBackgroundColor(Color.parseColor("#FEFEFE"));
+
+        bottomNavigation.setTranslucentNavigationEnabled(true);
+
+        bottomNavigation.setAccentColor(Color.parseColor("#F63D2B"));
+        bottomNavigation.setInactiveColor(Color.parseColor("#747474"));
+
+        //  Enables Reveal effect
+        bottomNavigation.setColored(false);
+
+        bottomNavigation.setCurrentItem(0);
+
+        bottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
+            @Override
+            public boolean onTabSelected(int position, boolean wasSelected) {
+                // Do something cool here...
+
+                Toast.makeText(MainViewer.this, "select", Toast.LENGTH_SHORT).show();
+
+                TransformTest t = tests[position];
+                if (artlib.requestTransform(src_img, t.transformType, t.intArgs, t.floatArgs)) {
+
+                    makeToast("Transform requested : " + transforms[t.transformType]);
+
+
+                } else {
+                    makeToast("Transform request failed" + transforms[t.transformType]);
+                }
+
+                return true;
+            }
+        });
     }
 
 
@@ -104,17 +157,17 @@ public class MainViewer extends AppCompatActivity {
         tests = artlib.getTestsArray();
         transforms = artlib.getTransformsArray();
 
-        for (TransformTest t : tests){
-            String str = transforms[t.transformType] + " : " + Arrays.toString(t.intArgs)+" : "+ Arrays.toString(t.floatArgs);
+        for (TransformTest t : tests) {
+            String str = transforms[t.transformType] + " : " + Arrays.toString(t.intArgs) + " : " + Arrays.toString(t.floatArgs);
             testsArray.add(str);
         }
 
     }
-    private void makeToast(String str){
+
+    private void makeToast(String str) {
         Toast.makeText(getBaseContext(), str,
                 Toast.LENGTH_SHORT).show();
     }
-
 
 
 }
